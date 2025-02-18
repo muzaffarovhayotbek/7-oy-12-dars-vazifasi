577a19ffffdf99b40214e4c9c905537791d377f0
muzaffarovhayotbek <hayotbek0511@gmail.com>
2025-02-19 00:10:41 +0500

    tsc

---
+++ compiled/index.js
@@ -0,0 +1,77 @@
+"use strict";
+// 2. Interface
+// 2. Topshiriq:
+// 5. Obyekt
+// 8. Topshiriq:
+// let Users = {
+//   name: 'hayotbek',
+//   age: 16,
+//   email: 'hayotbek@gmail.com'
+// }
+// console.log(Users);
+// 6. Funktsiyalar
+// 10. Topshiriq
+// function sumTwoNumber(firstNum, secondNum) {
+//   return firstNum + secondNum;
+// }
+// let firstNum = 10;
+// let secondNum = 20;
+// let res = sumTwoNumber(firstNum, secondNum);
+// console.log(res);
+// 7. Tuple
+// 12. Topshiriq:
+// 10. Type Alias
+// 18. Topshiriq:
+// type UserType  =  {
+//   name: string,
+//   age: number,
+//   isStudent: boolean
+// }
+// let User: UserType = {
+//   name : 'hayotbek',
+//   age: 16,
+//   isStudent: false,
+// }
+// console.log(User);
+// 8. Any
+// 14 . Topshiriq:
+// const any = [
+//   1,
+//   2,
+//   3,
+//   4,
+//   5,
+//   'salom',
+//   'xayr',
+//   {
+//     name: 'BMW',
+//     yaer: 2024,
+//     color: 'black',
+//     price: 105000,
+//   },
+// ];
+// for (let i = 0; i < any.length; i++) {
+//   if (typeof any[i] == 'number') {
+//     console.log(any[i]);
+//   }
+// }
+// let btn = [1, 2, 3, 4, 5, 6, 'salom', 'xary'];
+// for (let i = 0; i < btn.length; i++) {
+//   if (typeof btn[i] == 'string') {
+//     console.log(btn[i]);
+//   }
+// }
+// 19. Topshiriq:
+// type firstUser = {
+//   name: string
+// }
+// type SecondUser = {
+//   age: number
+// }
+// type User = firstUser & SecondUser
+// let user: User = {
+//   name: 'hayotbek',
+//   age: 16
+// }
+// console.log(user);
+// 20 .Topshiriq:
